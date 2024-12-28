9befa7fe753f8a9efc18843fa20b3f321735d4ad
Chillbroh <isharamadusanka410@gmail.com>
2024-12-28 22:39:59 +0530

    bg color added

--- frontend/src/sections/HeroSection.tsx
+++ frontend/src/sections/HeroSection.tsx
@@ -1,6 +1,9 @@
 export const HeroSection = () => {
   return (
-    <div className="h-[300px] w-full bg-primary-light">
+    <div
+      className="h-[300px] w-full bg-primary-light"
+      style={{ backgroundColor: "var(--color-primary-light)" }}
+    >
       <div>hehe</div>
     </div>
   );
