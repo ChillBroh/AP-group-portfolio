bee70cdb991c0741b19c864fe634a6312e01c3a3
Chillbroh <isharamadusanka410@gmail.com>
2025-01-25 17:21:28 +0530

    loading count removed

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -6,9 +6,9 @@ import {
   FaMobileAlt,
   FaLanguage,
   FaShippingFast,
-  FaUserFriends,
-  FaRegHandshake,
-  FaAward,
+  // FaUserFriends,
+  // FaRegHandshake,
+  // FaAward,
 } from "react-icons/fa";
 import Image from "next/image";
 import CountUp from "react-countup";
@@ -21,7 +21,7 @@ const HeroSection = () => {
       className="relative h-auto px-4 sm:px-8 md:px-16 lg:px-24 w-full bg-primary-light"
     >
       {/* Floating Counting Div */}
-      <div className="absolute w-2/4 h-auto md:-bottom-20 hidden bottom-1 left-1/2 transform -translate-x-1/2 z-10  md:flex flex-wrap items-center justify-between gap-6 px-6 py-4 bg-white shadow-lg rounded-lg">
+      {/* <div className="absolute w-2/4 h-auto md:-bottom-20 hidden bottom-1 left-1/2 transform -translate-x-1/2 z-10  md:flex flex-wrap items-center justify-between gap-6 px-6 py-4 bg-white shadow-lg rounded-lg">
         <div className="flex flex-col items-center gap-2">
           <FaUserFriends className="text-primary-base" size={32} />
           <div className="text-2xl font-bold">
@@ -43,7 +43,7 @@ const HeroSection = () => {
           </div>
           <div className="text-sm">Awards Won</div>
         </div>
-      </div>
+      </div> */}
 
       {/* Main Content Section */}
       <div className="flex flex-col lg:flex-row p-6 md:p-20">
