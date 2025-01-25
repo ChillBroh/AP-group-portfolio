75500b24301586751efdd39330611780de93f9a8
Chillbroh <isharamadusanka410@gmail.com>
2025-01-25 14:26:34 +0530

    added mobile and contact section

--- src/sections/ContactUsSection.tsx
+++ src/sections/ContactUsSection.tsx
@@ -1,10 +1,106 @@
+import { useEffect, useRef, useState } from "react";
+import { Button, Form, Input, Select } from "antd";
+const { Option } = Select;
+
+const onFinish = () => {};
+
 const ContactUsSection = () => {
+  const [animate, setAnimate] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setAnimate(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    const currentRef = sectionRef.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) observer.unobserve(currentRef);
+    };
+  }, []);
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
   return (
     <div
-      className="mt-28  px-4 sm:px-8 md:px-16 lg:px-36 w-full"
+      className="mt-28 px-4 sm:px-8 md:px-16 lg:px-36 w-full"
       id="contactUsSection"
+      ref={sectionRef}
     >
-      <div className="text-5xl font-bold text-center">Get In Touch</div>
+      <div className="text-5xl font-bold text-center mb-10">Get In Touch</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        {/* Contact Form */}
+        <div>
+          <Form name="contactUs" onFinish={onFinish}>
+            <div className="mb-3">Name</div>
+            <Form.Item
+              name={["user", "name"]}
+              rules={[{ required: true, message: "Please input your name" }]}
+            >
+              <Input />
+            </Form.Item>
+            <div className="mb-3">Mobile Number</div>
+            <Form.Item
+              name="phone"
+              rules={[
+                { required: true, message: "Please input your phone number!" },
+              ]}
+            >
+              <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
+            </Form.Item>
+            <div className="mb-3">Email</div>
+            <Form.Item
+              name={["user", "email"]}
+              rules={[
+                { type: "email", message: "Email is not a valid email" },
+                { required: true, message: "Please input your Email!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <div className="mb-3">Description</div>
+            <Form.Item
+              name={["user", "introduction"]}
+              rules={[{ required: true, message: "Please input your reason!" }]}
+            >
+              <Input.TextArea autoSize={{ minRows: 4, maxRows: 8 }} />
+            </Form.Item>
+            <div className="flex justify-center">
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
+
+        {/* Map Section */}
+        <div className="h-[300px] md:h-[400px] w-full  rounded-lg shadow-md overflow-hidden">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3225.8490258012025!2d139.8861897!3d36.0483903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018a51949d602a9%3A0xf04414eba35964a0!2zUC5BIEdyb3VwIOagquW8j-S8muekvg!5e0!3m2!1sen!2slk!4v1737793839612!5m2!1sen!2slk"
+            className="w-full h-full border-0"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 };
--- src/sections/MobileSection.tsx
+++ src/sections/MobileSection.tsx
@@ -1,7 +1,106 @@
+"use client";
+import Image from "next/image";
+import mobile1 from "../../public/images/mobile/mobile-1.jpg";
+import mobile2 from "../../public/images/mobile/mobile-2.jpg";
+import mobile3 from "../../public/images/mobile/mobile-3.jpg";
+import mobile4 from "../../public/images/mobile/mobile-4.jpg";
+import mobile5 from "../../public/images/mobile/mobile-5.jpg";
+
+import { useEffect, useRef, useState } from "react";
+
 const MobileSection = () => {
+  const [animate, setAnimate] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setAnimate(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    const currentRef = sectionRef.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) observer.unobserve(currentRef);
+    };
+  }, []);
+
   return (
-    <div className="mt-24  px-4 sm:px-8 md:px-16 lg:px-36 w-full">
-      <div className="text-5xl font-bold text-center">Mobile Section</div>
+    <div
+      className="mt-28 px-4 sm:px-8 md:px-16 lg:px-36 w-full"
+      id="mobileSection"
+      ref={sectionRef}
+    >
+      <div className="text-5xl font-bold text-center mb-10">
+        Mobile Services
+      </div>
+      <div
+        className={`grid md:grid-cols-3 gap-4 ${
+          animate ? "animate-fade-right" : "animate-fade-left"
+        } `}
+      >
+        {/* Left Section */}
+        <div className="md:col-span-1 col-span-2">
+          <Image
+            src={mobile1}
+            alt="Mobile Connection Services"
+            className="w-full h-full object-cover rounded-xl shadow-md"
+          />
+        </div>
+
+        {/* Right Section */}
+        <div className="col-span-2 grid grid-cols-2 gap-4">
+          <Image
+            src={mobile2}
+            alt="SIM and WIFI Campaign"
+            className="w-full h-full object-cover rounded-xl shadow-md"
+          />
+          <Image
+            src={mobile3}
+            alt="Mobile and Credit Card Promotion"
+            className="w-full h-full object-cover rounded-xl shadow-md"
+          />
+          <Image
+            src={mobile4}
+            alt="Customer Support Services"
+            className="w-full h-full object-cover rounded-xl shadow-md"
+          />
+          <Image
+            src={mobile5}
+            alt="Customer Support Services"
+            className="w-full h-full object-cover rounded-xl shadow-md"
+          />
+        </div>
+      </div>
+
+      {/* Description Section */}
+      <div className="mt-10 text-justify text-sm sm:text-base">
+        <p className="mb-4">
+          <strong>Organizing Mobile Events:</strong> We assist individuals who
+          come to Japan for work and study in obtaining mobile connection
+          facilities effortlessly.
+        </p>
+        <p className="mb-4">
+          <strong>SIM and WIFI Connection Campaign:</strong> Our services
+          include providing WIFI connection facilities directly to the doorsteps
+          of those arriving in Japan for work or study, ensuring seamless
+          internet access.
+        </p>
+        <p className="mb-4">
+          <strong>Mobile and Credit Card Promotion Campaign:</strong> We
+          simplify the process of acquiring a mobile connection and offer
+          support in applying for credit cards, making these essential
+          facilities easily accessible to our customers.
+        </p>
+      </div>
     </div>
   );
 };
